b308e111495b9394aad8e3462d13cdf64990af75
Sanket Bagad <alencolins@gmail.com>
2025-12-16 21:01:53 +0530

    fix: prisma generate

--- server/src/lib/db.ts
+++ server/src/lib/db.ts
@@ -1,25 +1,22 @@
 import "dotenv/config";
-import { default as PrismaClientModule } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 import { PrismaNeon } from "@prisma/adapter-neon";
 
-const PrismaClient = PrismaClientModule.PrismaClient || PrismaClientModule;
-const globalForPrisma = global as unknown as { prisma: InstanceType<typeof PrismaClient> };
+const globalForPrisma = globalThis as unknown as {
+  prisma?: PrismaClient;
+};
 
 function createPrismaClient() {
-  const connectionString = process.env.DATABASE_URL!;
-  const adapter = new PrismaNeon({ connectionString });
+  const adapter = new PrismaNeon({
+    connectionString: process.env.DATABASE_URL!,
+  });
+
   return new PrismaClient({ adapter });
 }
 
-let prisma: InstanceType<typeof PrismaClient>;
-
-if (process.env.NODE_ENV === "production") {
-  prisma = createPrismaClient();
-} else {
-  if (!globalForPrisma.prisma) {
-    globalForPrisma.prisma = createPrismaClient();
-  }
-  prisma = globalForPrisma.prisma;
-}
+const prisma =
+  process.env.NODE_ENV === "production"
+    ? createPrismaClient()
+    : (globalForPrisma.prisma ??= createPrismaClient());
 
 export default prisma;
